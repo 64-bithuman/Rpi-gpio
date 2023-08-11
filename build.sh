abb20b73e6df7e27d409f2e99e7362416ba2309b
Vinuda Liyanage <92595684+64-bithuman@users.noreply.github.com>
2023-08-11 22:28:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Compiling bcm2835.c ..."
 gcc -c bcm2835.c
 echo "Compiling main.cpp, gpio.cpp ..."
 g++ -c main.cpp gpio.cpp
-echo "Linking main.o, gpio.o, bcm2835.o to iocontrol.elf ..."
-g++ -o iocontrol.elf main.o gpio.o bcm2835.o
+echo "Linking main.o, gpio.o, bcm2835.o to rpi-gpio ..."
+g++ -o rpi-gpio main.o gpio.o bcm2835.o
 rm *.o*
 echo "Done."
